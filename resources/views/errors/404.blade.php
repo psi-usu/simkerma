d290154ef0a60fd67ebfcac05e16f6bb837b30c0
Surya Wijaya <kokorochi.zhou@gmail.com>
2017-07-14 11:40:29 +0700

    Add error pages

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<!--[if IE 8]>
+<html lang="en" class="ie8"> <![endif]-->
+<!--[if IE 9]>
+<html lang="en" class="ie9"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en"> <!--<![endif]-->
+
+<!-- START @HEAD  -->
+<head>
+    <!-- START @META  SECTION -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <title>Halaman Tidak Ditemukan | SIPUSTAHA - Error 404</title>
+    <!--/ END META SECTION -->
+
+    <!-- START @FONT  STYLES -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700" rel="stylesheet">
+    <!--/ END FONT STYLES -->
+
+    <!-- START @GLOBAL  MANDATORY STYLES -->
+    <link rel="stylesheet" href="{{url('assets/bootstrap/css/bootstrap.min.css')}}">
+    <!--/ END GLOBAL MANDATORY STYLES -->
+
+    <!-- START @PAGE  LEVEL STYLES -->
+    <!--/ END PAGE LEVEL STYLES -->
+
+<!-- START @THEME  STYLES -->
+    <link href="{{url('assets/css/reset.css')}}" rel="stylesheet">
+    <link href="{{url('assets/css/layout.css')}}" rel="stylesheet">
+    <link href="{{url('assets/css/components.css')}}" rel="stylesheet">
+    <link href="{{url('assets/css/plugins.css')}}" rel="stylesheet">
+    <link href="{{url('assets/css/themes/laravel.theme.css')}}" rel="stylesheet">
+    <link href="{{url('assets/css/themes/cyan.theme.css')}}" rel="stylesheet">
+    <link href="{{url('assets/css/custom.css')}}" rel="stylesheet">
+    <link href="{{url('assets/css/pages/error-page.css')}}" rel="stylesheet">
+    <!--/ END THEME STYLES -->
+
+<!-- START @IE  SUPPORT -->
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <![endif]-->
+<!--/ END IE SUPPORT -->
+</head>
+<!--/ END HEAD -->
+<body>
+
+<!--[if lt IE 9]>
+<p class="upgrade-browser">Upps!! You are using an <strong>outdated</strong> browser. Please <a
+        href="http://browsehappy.com/" target="_blank">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+
+<!-- START @ERROR  PAGE -->
+
+<div class="error-wrapper">
+    <h1>404!</h1>
+    <h3>Halaman yang anda tuju tidak ditemukan!</h3>
+    <h4>Halaman yang anda tuju mungkin sudah dihapus atau tidak tersedia lagi <br/> <br/></h4>
+</div>
+
+<!--/ END ERROR PAGE -->
+</body>
+<!--/ END BODY -->
+
+
+</html>
