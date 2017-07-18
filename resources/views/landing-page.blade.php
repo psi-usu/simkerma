e106b9029fc2697afcb334cb08d571df302a10e5
Surya Wijaya <kokorochi.zhou@gmail.com>
2017-07-18 09:56:05 +0700

    Update landing page

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -66,7 +66,7 @@
     <div class="container" id="maincontent" tabindex="-1">
         <div class="row">
             <div class="col-lg-12">
-                <img class="img-responsive" src="img/logo-1.png" alt="">
+                <span style="font-size: 100px;"><i class="fa fa-handshake"></i></span>
                 <div class="intro-text">
                     <h1 class="name">SIMKERMA</h1>
                     <hr class="star-light">
